b4f359be17a9edb64a2f1205fc1d3825d0f8f95e
Francisco Perez <fperezfrancisco4@gmail.com>
2025-02-13 21:01:19 -0800

    edited app for vercel deployment

--- notes-frontend/src/utils/constants.js
+++ notes-frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
-console.log(import.meta.env.BACK_END_URL);
+//console.log(import.meta.env.BACK_END_URL);
 
-export const BASE_URL = "https://notes-fullstack-nine.vercel.app/";
+export const BASE_URL = import.meta.env.BACK_END_URL;
